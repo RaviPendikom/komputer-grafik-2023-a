3e2afd39bcd8feec09df6d18a750bdd962048ac4
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-09-03 10:50:31 +0800

    Animate with random - Done

--- sketch.js
+++ sketch.js
@@ -1,15 +1,14 @@
-let x = 300;
-let y = 300;
-let radius = 100;
+// let x = 300;
+// let y = 300;
+// let radius = 100;
 
 function setup() {
   createCanvas(600, 600);
+  background(132, 49, 120);
 }
 
 function draw() {
-  background(132, 49, 120);
+  fill(random(255), random(255), random(255), random(255));
   noStroke();
-  fill(152, 200, 89);
-  circle(x, y, radius);
-  radius += 1;
+  circle(random(600), random(600), random(100));
 }
