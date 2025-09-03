609cf1d3d06e3ffc5e4c5e996df6b9ed813b8357
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-09-03 11:08:36 +0800

    Animate bounce ball - Done

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
-// let x = 300;
-// let y = 300;
-// let radius = 100;
+let x = 300;
+let y = 300;
+let radius = 100;
+let speedX = 2;
+let speedY = 5;
 
 function setup() {
   createCanvas(600, 600);
-  background(132, 49, 120);
 }
 
 function draw() {
-  fill(random(255), random(255), random(255), random(255));
+  background(132, 49, 120);
+  fill(20, 59, 112);
   noStroke();
-  circle(random(600), random(600), random(100));
+  circle(x, y, radius);
+  if (x + 50 >= 600 || x - 50 <= 0) {
+    speedX *= -1;
+  }
+
+  if (y + 50 >= 600 || y - 50 <= 0) {
+    speedY *= -1;
+  }
+  x += speedX;
+  y += speedY;
 }
