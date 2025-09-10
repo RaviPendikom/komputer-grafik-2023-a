667a3a3d72f90be841d2bc51c8d28e165fdd351a
RaviPendikom <ramaulvi@fkip.unmul.ac.id>
2025-09-10 10:45:28 +0800

    pmouse intro - Done

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
 function setup() {
   createCanvas(600, 600);
+  background("#476EAE");
 }
 
 function draw() {
-  background("#476EAE");
-  // line(0, 0, mouseX, mousey);
-  noStroke();
-  circle(mouseX, mouseY, 20);
+  line(pmouseX, pmouseY, mouseX, mouseY);
 }
